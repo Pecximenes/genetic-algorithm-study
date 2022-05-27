c939a71ce11c7ca4443965396064b44ff2287782
Pedro <pedroximenes76@gmail.com>
2022-05-26 21:58:17 -0300

    🔖 Released first version of the program (Needs update)

--- resolve_polynomial_functions/main.py
+++ resolve_polynomial_functions/main.py
@@ -1,12 +1,20 @@
 import random
 
 
-def calculate_functions(index_list, var_list, pol_list):
-    function_result = 0
+def calculate_functions(index_list, var_list, sol, des_sol):
+    result = 0
     for pos, i in enumerate(index_list):
-        function += var_list[pos] * pol_list**i
+        result += var_list[pos] * (sol[pos]**i)
 
-    return function_result
+    return result - des_sol
+
+
+def fitness(index_list, var_list, sol, des_sol):
+    answer = calculate_functions(index_list, var_list, sol, des_sol)
+    print(answer)
+    if answer == 0: return 99_999
+    else: return abs(1/answer)
+
 
 
 def generate_solutions(num):
@@ -27,13 +35,15 @@ variables_list = []
 polynom_dict = {}
 
 num_of_polynoms = int(input("Number of polynoms:"))
-for pos, i in enumerate(range(num_of_polynoms - 1, -1, -1)):
+for pos, i in enumerate(range(num_of_polynoms, 0, -1)):
     polynom = chr(97 + pos)
 
     result = int(input(f"Multiplicity of {polynom}**{i}: "))
 
+    if result != 0:
+        variables_list.append(result)
+
     index_list.append(i)
-    variables_list.append(result)
     polynom_dict[polynom] = []
 
 
@@ -42,6 +52,42 @@ print(variables_list)
 print(polynom_dict)
 
 solutions = generate_solutions(num_of_polynoms)
-
-
 print(solutions)
+
+desired_solution = int(input("Desired solution: "))
+
+for i in range(10000):
+    rankedsolutions = []
+    for solution in solutions:
+        weight = fitness(index_list, variables_list, solution, desired_solution)
+        print(weight)
+        rankedsolutions.append([weight, solution])
+    
+    rankedsolutions.sort()
+    rankedsolutions.reverse()
+
+    print(f"=== Gen {i} best solutions ===")
+    print(rankedsolutions[0])
+
+    if rankedsolutions[0][0] > 9999:
+        break
+
+    bestsolutions = rankedsolutions[:100]
+
+    elements = []
+    for b_sol in bestsolutions:
+        for i in b_sol[1]:
+            elements.append(i)
+    
+    new_generation = []
+    for _ in range(1000):
+        e1 = random.choice(elements) * random.uniform(0.99, 1.01)
+        e2 = random.choice(elements) * random.uniform(0.99, 1.01)
+        e3 = random.choice(elements) * random.uniform(0.99, 1.01)
+
+        new_generation.append([e1, e2, e3])
+    
+    solutions = new_generation
+
+print("Final result:")
+print(calculate_functions(index_list, variables_list, solution, desired_solution) + desired_solution)
